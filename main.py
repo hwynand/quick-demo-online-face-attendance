41e473bfd31e2736c3fd1812ee225c81f52af8bf
huongpx <huong.px01@gmail.com>
2023-04-03 23:56:49 +0700

    ✨ connect to local sqlite db

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from models import Base
 
 app = FastAPI()
+engine = create_engine("sqlite:///app.db", pool_pre_ping=True)
+DBSession = Session(bind=engine, autoflush=False, autocommit=False)
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -14,3 +22,13 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/signup", response_class=HTMLResponse)
+async def signup(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
